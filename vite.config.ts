adc69ece8991d0849edf73975357879ac509d2f3
Renan Schmitt <renanschmitt.ti@gmail.com>
2026-04-15 01:06:25 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ESSA LINHA É OBRIGATÓRIA para o visual carregar no GitHub Pages
+  base: "/gearhubmkt/",
   server: {
     host: "::",
     port: 8080,
