1ff38950d2d2cf913aa686b72e1237c00742288e
Renan Schmitt <renanschmitt.ti@gmail.com>
2026-04-15 00:43:01 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Adicione esta linha abaixo para o deploy no GitHub Pages funcionar
+  base: "/gearhubmkt/",
   server: {
     host: "::",
     port: 8080,
